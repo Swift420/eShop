515a648de9113cfbe62f480bc052fef66b9581b9
Swift420 <apollosdavid95@gmail.com>
2022-12-29 10:51:08 +0200

    Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,29 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/products/:id",
+    element: <div>Category</div>,
+  },
+
+  {
+    path: "/product/:id",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 function App() {
   return (
     <div >
-
+          <RouterProvider router={router} />
     </div>
   );
 }
